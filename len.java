c4fd4863c95708d3d284e263d22a3a845965e227
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-03 05:59:20 -0700

    ouieiwuo i
    ml.kcjlksdjsd

---
+++ len.java
@@ -0,0 +1,21 @@
+package prime;
+import java.util.Scanner;
+public class powerv 
+{
+	public static void main( String[] args)
+	{
+		String s;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter name:");
+
+		s = in.nextLine();
+
+		System.out.print("String Length :" );
+
+		System.out.println(s.length());
+
+		}
+
+		}
