feabeb4cd9ee737d95cfed26f7e3eb9faefcec27
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-03 05:52:53 -0700

    hfjkl
    eoiopipoi

---
+++ two.java
@@ -0,0 +1,21 @@
+package prime;
+import java.util.Scanner;
+public class powerv 
+{
+	public static void main( String[] args)
+	{
+	Scanner in=new Scanner(System.in);
+    System.out.println("Enter the number : ");
+    int num = in.nextInt();
+
+    int other = 1;  
+    if(((~num) & 1) == 1)  
+    {  
+        System.out.println("The number is a power of two");  
+    }  
+    else  
+    {
+        System.out.println("The number is a  NOT A power of two");  
+    }
+}  
+} 
